1d30b458abd21e3b6dc29b489a6ade294f234264
Rogelio Alberto <i@rog.mx>
2017-03-12 22:33:00 -0500

    Logo component

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,10 +11,13 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import messages from './messages'
 
+import Logo from '../../components/Logo'
+
 export default class HomePage extends React.PureComponent {
   render () {
     return (
       <h1>
+        <Logo />
         <FormattedMessage {...messages.header} />
       </h1>
     )
