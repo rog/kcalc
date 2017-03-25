239bd891dd47397aca6e0c2e395db210dbdc182a
Rogelio Alberto <i@rog.mx>
2017-03-24 22:33:00 -0500

    Create Lang container

---
+++ app/containers/LocaleToggle/messages.js
@@ -0,0 +1,17 @@
+/*
+ * LocaleToggle Messages
+ *
+ * This contains all the text for the LanguageToggle component.
+ */
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  en: {
+    id: 'kcalc.containers.LocaleToggle.en',
+    defaultMessage: 'en'
+  },
+  es: {
+    id: 'kcalc.containers.LocaleToggle.es',
+    defaultMessage: 'es'
+  }
+})
